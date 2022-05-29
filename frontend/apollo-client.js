651b450261398b0328e804f25125bf4b0355138a
joseniquen08 <j.armando0807@gmail.com>
2022-05-29 00:18:16 -0500

    front-> environment variables fixed

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -2,9 +2,11 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const cache = new InMemoryCache();
 
+const uri = process.env.NEXT_PUBLIC_SERVER_URI + '/graphql';
+
 const client = new ApolloClient({
   cache: cache,
-  uri: `https://vocaccion-backend-production.up.railway.app/graphql`,
+  uri: uri,
 });
 
 export default client;
