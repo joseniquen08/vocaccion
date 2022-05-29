1edaba9b16714b334d6051cd8cf512043ea9b158
joseniquen08 <j.armando0807@gmail.com>
2022-05-28 22:58:35 -0500

    front-> environment variables fixed

--- frontend/apollo-client.js
+++ frontend/apollo-client.js
@@ -4,7 +4,7 @@ const cache = new InMemoryCache();
 
 const client = new ApolloClient({
   cache: cache,
-  uri: `${process.env.MONGODB_URI}/graphql`,
+  uri: `${process.env.SERVER_URI}/graphql`,
 });
 
 export default client;
