401af06433874c890cfad34699da78e8a4d65b8a
joseniquen08 <j.armando0807@gmail.com>
2022-09-10 11:13:15 -0500

    backend-> fix: dependencies updated

--- backend/src/shared/controllers/uploadControllers.ts
+++ backend/src/shared/controllers/uploadControllers.ts
@@ -5,9 +5,9 @@ import { cloudinaryService } from '../services/uploadServices';
 
 export const uploadImageController = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { url } = await cloudinaryService(req.file.path);
+    const { url } = await cloudinaryService(req.file!.path);
     if (!url) throw new Error('url not found');
-    await fs.remove(req.file.path);
+    await fs.remove(req.file!.path);
     res.status(201).send({ url });
   } catch (error: any) {
     next(new ApplicationError(401, `${error.message}`));
